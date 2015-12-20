949ac8b77f265f50252c971d79011b8b73c16b0e
zarinfam.s <zarinfam.s@gmail.com>
2015-12-20 15:37:14 +0330

    create project

---
+++ src/main/java/com/rayan/course/Main.java
@@ -0,0 +1,11 @@
+package com.rayan.course;
+
+/**
+ * Created by saeed on 12/13/15.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
