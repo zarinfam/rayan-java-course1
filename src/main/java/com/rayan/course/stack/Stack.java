22245e6157442e07d4650cd72d9743834f797838
zarinfam.s <zarinfam.s@gmail.com>
2016-02-23 15:02:34 +0330

    add draft implementation of stack.

---
+++ src/main/java/com/rayan/course/stack/Stack.java
@@ -0,0 +1,20 @@
+package com.rayan.course.stack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by saeed on 2/23/16.
+ */
+public class Stack {
+
+    private List<Integer> stack = new ArrayList<>();
+
+    public void push(int element){
+
+    }
+
+    public int pop(){
+        return 0;
+    }
+}
